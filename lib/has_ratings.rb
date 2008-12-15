0a2dd0172f7cb93f15d350c3d0c3f212cba60b0b
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-12-14 21:30:53 -0500

    Remove the PluginAWeek namespace

--- lib/has_ratings.rb
+++ lib/has_ratings.rb
@@ -1,54 +1,52 @@
-module PluginAWeek #:nodoc:
-  # Adds a generic implementation for dealing with ratings
-  module HasRatings
-    module MacroMethods
-      # Creates the following association:
-      # * +ratings+ - All ratings associated with the current record
-      # 
-      # This association assumes that it is being created on the +ratable+
-      # model.  If you want to create an association on the +rater+ model, it will
-      # have to be created manually like so:
-      # 
-      #   has_many :ratings, :as => :rater
-      # 
-      # == Creating new ratings
-      # 
-      # Creating new ratings is very similar to creating other records within
-      # a has_many association:
-      # 
-      #   user = User.find(1)
-      #   
-      #   video = Video.find_by_name('The Shawshank Redemption')
-      #   video.ratings.create(:rater => user, :value => 'excellent')
-      #   video.ratings.average   # => 5
-      def has_ratings
-        has_many  :ratings,
-                    :as => :ratable,
-                    :extend => RatingExtension
-      end
+# Adds a generic implementation for dealing with ratings
+module HasRatings
+  module MacroMethods
+    # Creates the following association:
+    # * +ratings+ - All ratings associated with the current record
+    # 
+    # This association assumes that it is being created on the +ratable+
+    # model.  If you want to create an association on the +rater+ model, it will
+    # have to be created manually like so:
+    # 
+    #   has_many :ratings, :as => :rater
+    # 
+    # == Creating new ratings
+    # 
+    # Creating new ratings is very similar to creating other records within
+    # a has_many association:
+    # 
+    #   user = User.find(1)
+    #   
+    #   video = Video.find_by_name('The Shawshank Redemption')
+    #   video.ratings.create(:rater => user, :value => 'excellent')
+    #   video.ratings.average   # => 5
+    def has_ratings
+      has_many  :ratings,
+                  :as => :ratable,
+                  :extend => RatingExtension
     end
-    
-    module RatingExtension
-      # Gets the average of all the ratings, up to a precision of 2 decimals.
-      # If no ratings have ever been made, then 0.0 is returned.
-      # 
-      # For example,
-      # 
-      #   video = Video.find_by_name('The Shawshank Redemption')
-      #   video.ratings.map {|rating| rating.value.value}   # => [4, 5, 5]
-      #   video.ratings.average                             # => 4.67
-      def average
-        if empty?
-          0.0
-        else
-          average = inject(0) {|total, rating| total += rating.value.to_i} / size.to_f
-          average.round(2)
-        end
+  end
+  
+  module RatingExtension
+    # Gets the average of all the ratings, up to a precision of 2 decimals.
+    # If no ratings have ever been made, then 0.0 is returned.
+    # 
+    # For example,
+    # 
+    #   video = Video.find_by_name('The Shawshank Redemption')
+    #   video.ratings.map {|rating| rating.value.value}   # => [4, 5, 5]
+    #   video.ratings.average                             # => 4.67
+    def average
+      if empty?
+        0.0
+      else
+        average = inject(0) {|total, rating| total += rating.value.to_i} / size.to_f
+        average.round(2)
       end
     end
   end
 end
 
 ActiveRecord::Base.class_eval do
-  extend PluginAWeek::HasRatings::MacroMethods
+  extend HasRatings::MacroMethods
 end
